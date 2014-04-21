910450e2b268bcf151d15c395eb06d94303b46a4
XuefengZhai <xzhai@cmu.edu>
2014-04-20 20:38:16 -0400

    Adding simple login/psw judge

--- app/src/main/java/com/cmu/as/socket/ServerThread.java
+++ app/src/main/java/com/cmu/as/socket/ServerThread.java
@@ -4,13 +4,18 @@ import android.util.Log;
 
 import com.cmu.as.entities.patient.Patient;
 
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.StreamCorruptedException;
 import java.net.Inet4Address;
 import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Enumeration;
 
 /**
@@ -21,20 +26,28 @@ public class ServerThread extends Thread {
     private ServerSocket serverSocket;
     private Socket clientSocket;
     private ObjectOutputStream out;
+    private ObjectInput in;
 
-    public void run(){
+    public void run() {
 
-        try{
-            Log.i("","Enter Run");
+        try {
+            Log.i("", "Enter Run");
             // Open a server socket listening on port 8888
             InetAddress addr = InetAddress.getByName(getLocalIpAddress());
             serverSocket = new ServerSocket(8888, 0, addr);
             clientSocket = serverSocket.accept();
 
-            Log.i("","After accept");
+            Log.i("", "After accept");
 
 
             out = new ObjectOutputStream(clientSocket.getOutputStream());
+            in = new ObjectInputStream(clientSocket.getInputStream());
+
+            String namePsw = (String) in.readObject();
+
+            while (!(namePsw.equals("1\n2"))) {
+                out.writeObject("0");
+            }
 
             Patient pat = new Patient();
 
@@ -55,12 +68,18 @@ public class ServerThread extends Thread {
             clientSocket.close();
 
             out.close();
-
-        } catch(Exception e) {
-
-            e.printStackTrace();
+        } catch (UnknownHostException e1) {
+            e1.printStackTrace();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        } catch (ClassNotFoundException e1) {
+            e1.printStackTrace();
+        } catch (Exception e1) {
+            e1.printStackTrace();
         }
     }
+
+
 
     private String getLocalIpAddress() throws Exception {
         String resultIpv6 = "";
